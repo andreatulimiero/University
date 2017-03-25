ee36c155e63d67ec809ea5521265465e9d83dd98
Andrea Tulimiero <tulimiero.andrea@gmail.com>
2017-03-25 19:20:06 +0100

    Added ASD exercise 2

---
+++ ASD/es_2/max_level.c
@@ -0,0 +1,19 @@
+include <stdlib.h>
+#include <stdio.h>
+
+#include "queue.h"
+#include "max_level.h"
+#include "tree.h"
+
+int max(int a, int b) { return a > b ? a : b; }
+
+int max_level(node_tree * n) {
+    node_tree* s = n;
+    int keys_sum = get_node_info(s);
+    while ((s = get_next_sibling(s)) != NULL)
+        keys_sum += get_node_info(s);
+
+    node_tree* first_child = get_first_child(n);
+    if (first_child == NULL) return keys_sum;
+    return max(keys_sum, max_level(first_child));
+}
