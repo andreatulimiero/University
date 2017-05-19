47e16e0c86b7e440abff24e4249635d80eb0911b
Andrea Tulimiero <Andrea Tulimiero>
2017-05-19 20:57:06 +0200

    Enhanced tester file

--- ASD/Exams/20150715/problem_2/test.sh
+++ ASD/Exams/20150715/problem_2/test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash 
 
-./main < albero_bello.in
-echo '[Expected] true'
+printf '=== Testing albero_bello === \n\n'
 
+# Test with true input
+./main < albero_bello.in
+printf '[Expected] true\n\n'
+
+# Test with false input
 ./main < not_albero_bello.in
-echo '[Expected] false'
+printf '[Expected] false\n\n'
+
+printf '=== End of test === \n\n'
